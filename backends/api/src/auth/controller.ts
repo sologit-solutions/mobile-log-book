6723ed0e3dc81fb43c06bfc3d314b6a53b4e5aec
Tristan AMIOTTE-SUCHET <tamiottesuchet@gmail.com>
2025-10-24 16:41:45 +0300

    fix: try prettier in the project

--- backends/api/src/auth/controller.ts
+++ backends/api/src/auth/controller.ts
@@ -1,13 +1,10 @@
 import express from "express";
 
 // POST /login
-export const login = (req: express.Request, res: express.Response) => {
-};
+export const login = (req: express.Request, res: express.Response) => {};
 
 // POST /logout
-export const logout = (req: express.Request, res: express.Response) => {
-};
+export const logout = (req: express.Request, res: express.Response) => {};
 
 // POST /signup
-export const signup = (req: express.Request, res: express.Response) => {
-};
+export const signup = (req: express.Request, res: express.Response) => {};
