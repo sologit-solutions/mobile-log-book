97c34e4d7a4f4e93b14b8f0bd6eb7c2f2b742a04
Kenneth Andersson <113473148+keanderss@users.noreply.github.com>
2025-10-28 08:58:40 +0200

    API Changes and formatting

--- backends/api/prisma.config.ts
+++ backends/api/prisma.config.ts
@@ -1,8 +1,8 @@
-import path from 'node:path'
+import path from "node:path";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: path.join('prisma'),
+  schema: path.join("prisma"),
   migrations: {
     path: "prisma/migrations",
   },
