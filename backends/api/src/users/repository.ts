f954a511926d4cb945d3ab57a55098eafa611f70
keanderss <kenzu.2000172@gmail.com>
2026-02-13 07:36:54 +0200

    Refactor error handling

--- backends/api/src/users/repository.ts
+++ backends/api/src/users/repository.ts
@@ -1,4 +1,3 @@
-import { Prisma } from "@prisma/client";
 import { prisma } from "../configs/db.ts";
 import type { User } from "../types/user.ts";
 import type { Result } from "../types/result.ts";
@@ -6,71 +5,29 @@ import type { Result } from "../types/result.ts";
 export const createUser = async (
   user: Pick<User, "username" | "email" | "hash">,
 ): Promise<Result<{ userId: string }>> => {
-  try {
-    const data = await prisma.user.create({
-      data: user,
-    });
+  const data = await prisma.user.create({
+    data: user,
+  });
 
-    return { success: true, data: { userId: data.id } };
-  } catch (e) {
-    if (e instanceof Prisma.PrismaClientKnownRequestError) {
-      if (e.code !== "P2002") {
-        return { success: false, error: { code: e.code } };
-      }
-
-      const target = e.meta?.target as string[];
-
-      if (target[0] === "username") {
-        return {
-          success: false,
-          error: {
-            code: e.code,
-            message: "This username is already in use.",
-          },
-        };
-      }
-
-      if (target[0] === "email") {
-        return {
-          success: false,
-          error: {
-            code: e.code,
-            message: "This email address is already in use.",
-          },
-        };
-      }
-    }
-
-    return { success: false, error: { message: "Unknown error" } };
-  }
+  return { success: true, data: { userId: data.id } };
 };
 
 export const getUserByEmail = async (
   user: Partial<Pick<User, "username" | "email">>,
 ): Promise<Result<User>> => {
-  try {
-    const result = await prisma.user.findFirstOrThrow({
-      where: { email: user.email },
-    });
+  const result = await prisma.user.findFirstOrThrow({
+    where: { email: user.email },
+  });
 
-    return { success: true, data: result };
-  } catch (e) {
-    // TODO: error handling
-    return { success: false, error: { message: "Unknown error" } };
-  }
+  return { success: true, data: result };
 };
 
 export const getUserByUsername = async (
   user: Partial<Pick<User, "username" | "email">>,
 ): Promise<Result<User>> => {
-  try {
-    const result = await prisma.user.findFirstOrThrow({
-      where: { username: user.username },
-    });
+  const result = await prisma.user.findFirstOrThrow({
+    where: { username: user.username },
+  });
 
-    return { success: true, data: result };
-  } catch (e) {
-    // TODO: error handling
-    return { success: false, error: { message: "Unknown error" } };
-  }
+  return { success: true, data: result };
 };
--- backends/api/src/utils/requestUtils.ts
+++ backends/api/src/utils/requestUtils.ts
@@ -2,6 +2,7 @@ import type { Response } from "express";
 import type { Result } from "./../types/result.ts";
 import { ZodType } from "zod";
 import logger from "../utils/logger.ts";
+import handlePrismaError from "./prismaErrorHandler.ts";
 
 type ContextWithInput<T> = {
   res: Response;
@@ -17,6 +18,12 @@ type ContextWithoutInput = {
 
 type HandlerContext<T> = ContextWithInput<T> | ContextWithoutInput;
 
+const defaultErrorResponse: Result<object> = {
+  success: false,
+  status: 500,
+  error: { message: "Internal server error" },
+};
+
 /**
  * Takes a context object as an input. The context of the
  * request contains the response object and the function
@@ -30,18 +37,20 @@ type HandlerContext<T> = ContextWithInput<T> | ContextWithoutInput;
  * If an error occurs in the function call, the function
  * catches the error and returns a 500 response to the
  * client and logs the errors.
+ *
+ * @template T
+ * @param {HandlerContext<T>} context
+ * @return {Promise<Response<object>>}
  */
 const handleRequest = async <T>(
   context: HandlerContext<T>,
 ): Promise<Response<object>> => {
   try {
-    // Destructure the response object from the context
+    let result: Result<object> = defaultErrorResponse;
     const { res } = context;
 
     // Check if input exists on handlerContext
-    let serviceResult;
     if ("input" in context) {
-      // Destructure the context
       const { input, validator, fun } = context;
 
       // Read and validate the request
@@ -53,37 +62,65 @@ const handleRequest = async <T>(
       }
 
       // Call the service function
-      serviceResult = await fun(validationResult.data);
+      result = await fun(validationResult.data);
     } else {
-      // Destructure function from the context
       const { fun } = context;
 
       // Call the service function
-      serviceResult = await fun();
+      result = await fun();
     }
 
-    // Check if the service set custom status code
-    if (serviceResult.status) {
-      res.status(serviceResult.status);
-    }
-
-    // Check if the service function completed successfully
-    if (serviceResult.success) {
-      return res.json(serviceResult);
-    }
-
-    // Return errors
-    return res.json(serviceResult.error);
-  } catch (e) {
-    // Log error
-    logger.error(e);
-
-    // Destructure the context
-    const { res } = context;
-
-    // Return 500
-    return res.status(500).json({ error: "Internal server error" });
+    return send(res, result);
+  } catch (error) {
+    // handle errors
+    return await errorHandler(error, context);
   }
+};
+
+/**
+ * Error handler function that can be extended by
+ * adding more custom error handlers.
+ *
+ * @template T
+ * @param {unknown} error
+ * @param {HandlerContext<T>} context
+ * @return {Promise<Response<object>>}
+ */
+const errorHandler = async <T>(
+  error: unknown,
+  context: HandlerContext<T>,
+): Promise<Response<object>> => {
+  let result: Result<object> = defaultErrorResponse;
+  logger.error(error);
+  const { res } = context;
+
+  // ----- Error handlers begin -----
+
+  result = await handlePrismaError(error, result);
+
+  // ------ Error handlers end ------
+
+  return send(res, result);
+};
+
+/**
+ * Used to manipulate the response object before sending,
+ * adds status to the response object if defined in the result.
+ *
+ * Can be further extended along with the result type if
+ * necessary.
+ *
+ * @param {Response} res
+ * @param {Result<object>} result
+ * @return {Response<object>}
+ */
+const send = (res: Response, result: Result<object>): Response<object> => {
+  // Check if the result set custom status code
+  if (result.status) {
+    res.status(result.status);
+  }
+
+  return res.json(result);
 };
 
 export default handleRequest;
