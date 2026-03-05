dc875696dcfc952dc75a019ecb6a8204ce3e59b5
Tristan AMIOTTE-SUCHET <tamiottesuchet@gmail.com>
2026-03-05 14:56:32 +0200

    correct env var names
    + remove few unused importations

--- backends/api/src/users/service.ts
+++ backends/api/src/users/service.ts
@@ -4,10 +4,7 @@ import * as auth from "../utils/authUtils.ts";
 import emailValidator from "./validators/emailValidator.ts";
 import { ENV } from "../configs/env.ts";
 import type { Result } from "../types/result.ts";
-import { get } from "http";
-import { th } from "zod/locales";
 import logger from "../utils/logger.ts";
-import { date } from "zod";
 
 export const createUser = async (
   input: Pick<User, "username" | "email"> & { password: string },
